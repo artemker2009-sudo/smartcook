40048d14f0652e9ae787da0386976529dda53859
Artem <artem@MacBook-Air-Artem.local>
2026-02-04 22:06:06 +0300

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// 1. Добавили импорт аналитики
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        
+        {/* 2. Вставили компонент аналитики в конец body */}
+        <Analytics />
       </body>
     </html>
   );

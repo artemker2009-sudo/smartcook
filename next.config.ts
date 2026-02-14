64ec36372284c04558cc66c559f8150385b3590a
Artem <artem@MacBook-Air-Artem.local>
2026-02-14 12:57:43 +0300

    Добавил иконки приложения

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,28 @@
+import withPWAInit from 'next-pwa';
+
+// Настройка PWA
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // Выключаем PWA в режиме разработки, чтобы не кэшировалось лишнее
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* Мы просим Next.js не останавливать сборку из-за ошибок линтера */
   eslint: {
     ignoreDuringBuilds: true,
   },
-  /* Мы просим игнорировать ошибки типов при сборке (раз локально работает) */
+  /* Мы просим игнорировать ошибки типов при сборке */
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Дополнительная оптимизация для изображений (пригодится)
+  images: {
+    unoptimized: true, // Иногда нужно для корректной работы иконок в PWA
+  }
 };
 
-export default nextConfig;
+// Оборачиваем конфиг в PWA
+export default withPWA(nextConfig);
